0d2f9873ed4ed0baf4933364a9758fbfd155fb96
Simon Kusterer <simon@soped.com>
2012-09-25 12:08:36 +0200

    initial

---
+++ dist/smart-imagemap.js
@@ -0,0 +1,15 @@
+/*! Smart Imagemap - v0.1.0 - 2012-09-25
+* https://github.com/xat/smart-imagemap
+* Copyright (c) 2012 Simon Kusterer; Licensed MIT */
+
+(function($) {
+
+  // Collection method.
+  $.fn.awesome = function() {
+    return this.each(function() {
+
+
+    });
+  };
+
+}(jQuery));
---
+++ src/smart-imagemap.js
@@ -0,0 +1,17 @@
+/*
+ * smart-imagemap
+ * https://github.com/xat/smart-imagemap
+ *
+ * Copyright (c) 2012 Simon Kusterer
+ * Licensed under the MIT license.
+ */
+
+(function($) {
+
+  $.fn.smartImagemap = function() {
+    return this.each(function() {
+      $(this).addClass('awesomesauce');
+    });
+  };
+
+}(jQuery));
